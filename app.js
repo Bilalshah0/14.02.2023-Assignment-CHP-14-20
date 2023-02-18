08d0986d89d9f39dd409acefc46f2772d953c9bf
Bilalshah0 <121691643+Bilalshah0@users.noreply.github.com>
2023-02-18 19:15:08 +0500

    uploaded assignment

---
+++ app.js
@@ -0,0 +1,97 @@
+// *************** Chapter No.14-16 ***************
+
+
+// *************** Question no.01 ***************
+
+    // var studentNames = [];
+
+// *************** Question no.02 ***************
+
+  // var studentNames = [];
+
+// *************** Question no.03 ***************
+
+    // var stringArr = ["Bilal" , "Shah"]
+
+// *************** Question no.04 ***************
+
+    // var numberss = [1, 2, 3, 4, 5];
+
+// *************** Question no.05 ***************
+
+    // var booleanArr = [true, false];
+
+// *************** Question no.06 ***************
+
+    // var mixedArr = [ "Bilal", 1 , true , {name: " Shah "}];
+
+// *************** Question no.07 ***************
+
+    // var educationQualificationsPakistan = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+    // document.write("<h2>Qualifications:</h2>");
+    // document.write("<ol>");
+    // for (var i = 0; i < educationQualificationsPakistan.length; i++) {
+    // document.write("<li>" + educationQualificationsPakistan[i])
+    // }
+
+
+// *************** Question no.08 ***************
+
+    // var studentNames = ["John", "Jane", "Jack"];
+    // var studentScores = [450, 420, 480];
+
+    // for (var i = 0; i < studentNames.length; i++) {
+    // var percentage = (studentScores[i] / 500) * 100;
+    // document.write(studentNames[i] + " scored " + studentScores[i] + " marks and has a percentage of " + percentage + " % ")
+    // }
+
+// *************** Question no.09 ***************
+
+
+
+
+
+// *************** Question no.10 ***************
+
+    // var studentScores = [68, 90, 50, 40, 80];
+
+    // studentScores.sort(function(a, b){
+    // return a-b;
+    // });
+
+    // console.log(studentScores);
+
+
+// *************** Question no.11 ***************
+
+    // let cities = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia", "Phoenix", "San Antonio", "San Diego", "Dallas", "San Jose"];
+
+    // let selectedCities = cities.slice(0, 3);
+
+    // console.log(selectedCities); 
+
+
+// *************** Question no.12 ***************
+
+    // var arr = ["This ", " is ", " my ", " cat"];
+    // var singleString = arr.join("");
+    // console.log(singleString);
+
+
+// *************** Question no. ***************
+
+
+
+
+// *************** Question no. ***************
+
+
+
+
+
+// *************** Question no. ***************
+
+
+
+// *************** Chapter No. 16-20 **************
